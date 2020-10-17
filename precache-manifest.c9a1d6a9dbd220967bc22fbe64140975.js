83576f2ee0967180888258b18152e8c3249292ed
anton <anton235g@gmail.com>
2020-10-17 14:45:26 +0300

    Updates

---
+++ precache-manifest.c9a1d6a9dbd220967bc22fbe64140975.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d5635ef53a4151dcf859861e446a1cf",
+    "url": "/DicePoker/register-service-worker.js"
+  },
+  {
+    "revision": "59b8eb02868c2d17195c",
+    "url": "/DicePoker/static/js/app.608c7cd6.chunk.js"
+  },
+  {
+    "revision": "37283b275d35a93b3e3a",
+    "url": "/DicePoker/static/js/runtime~app.6f89e9ff.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/DicePoker/static/js/2.53df8f94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1439b15f0c9d388b31d6",
+    "url": "/DicePoker/static/js/2.53df8f94.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/DicePoker/serve.json"
+  },
+  {
+    "revision": "77f7910094a8b19f5e25284ff7721dd4",
+    "url": "/DicePoker/manifest.json"
+  },
+  {
+    "revision": "14949880dda86db2f49b6c9b133e6361",
+    "url": "/DicePoker/index.html"
+  },
+  {
+    "revision": "b27287954a8393ef9c70073b2fb0b988",
+    "url": "/DicePoker/favicon.ico"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/DicePoker/favicon-32.png"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/DicePoker/favicon-16.png"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/DicePoker/expo-service-worker.js"
+  }
+];
